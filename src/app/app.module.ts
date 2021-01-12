c0c7fe068be06d25d03c845cbd30af9bbacc1225
Tomas Kotlar <tomas.kotlar@red-opal.com>
2021-01-12 15:49:49 +0000

    CI(app): Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,21 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BookComponent } from './book/book.component';
+import { MaterialsModule } from "./shared/materials.module";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    MaterialsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
