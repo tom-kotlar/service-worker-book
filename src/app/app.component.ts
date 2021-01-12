2d86f97cb03961fc01d97993d16387c446b88b93
Tomas Kotlar <tomas.kotlar@red-opal.com>
2021-01-12 18:49:55 +0000

    Ref(app.ts): Serviceworker api- relopad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'service-worker';
+export class AppComponent implements OnInit {
+
+  constructor(private swUpdate: SwUpdate) { }
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm("New Version is Availible, Load new Version?")) {
+          window.location.reload()
+        }
+      })
+    }
+  }
 }
