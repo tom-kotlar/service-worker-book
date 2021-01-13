5a52ba3a5f60e23dd30607ecd1c4c6e62b6d1093
Tomas Kotlar <tomas.kotlar@red-opal.com>
2021-01-13 23:04:50 +0000

    CI(enviroments.ts): Store paths to Containers

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  azureContainers: {
+    baseUrl: 'blob.core.windows.net',
+    storage: 'testworker',
+    container: 'books'
+  }
+
 };
 
 /*
